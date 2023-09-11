7d2b93f48d45f34007b79cb2d9a07d10c2987088
Nik <nikitozer25@gmail.com>
2023-09-11 15:31:24 +0700

    .

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,4 @@
-function openButton(el) {
-
-var firstChild = el.firstChild;
-var lastChild = el.lastChild;
-
-el.classList.toggle("expand");
-if (lastChild.style.display === "none") {
-	lastChild.style.display = "inline";
-	firstChild.style.float = "left";
-
-}
-else{
-	lastChild.style.display = "none";
-	firstChild.style.float = "none";
-}
-
-
+document.querySelectorAll('.mybtn').forEach(o => o.addEventListener('click', openButton))
+function openButton() {
+  this.classList.toggle("expand");
 }
